1d36a4885ebdda7ee2c34dfe63d60880c320b3a5
Barry Brands <barrybrands02@hotmail.com>
2025-04-14 15:45:43 +0200

    readability

--- lib/EventListener/ObjectUpdatedEventListener.php
+++ lib/EventListener/ObjectUpdatedEventListener.php
@@ -40,7 +40,7 @@ class ObjectUpdatedEventListener implements IEventListener
         $synchronizations = $this->synchronizationService->findAllBySourceId(register: $object->getRegister(), schema: $object->getSchema());
         foreach ($synchronizations as $synchronization) {
             try {
-                $this->synchronizationService->synchronize($synchronization, false, true, $object);
+                $this->synchronizationService->synchronize(synchronization: $synchronization, isTest: false, force: true, object: $object);
             } catch (\Exception $e) {
                 $this->logger->error('Failed to process object event: ' . $e->getMessage() . ' for synchronization ' . $synchronization->getId(), [
                     'exception' => $e,
