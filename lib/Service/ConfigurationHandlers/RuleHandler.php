c5ee5819eaf84beabd63895e72d2922d14f0b6bd
Ruben van der Linde <rubenvdlinde@gmail.com>
2025-05-18 23:02:22 +0200

    Slug friendly rules

--- lib/Service/ConfigurationHandlers/RuleHandler.php
+++ lib/Service/ConfigurationHandlers/RuleHandler.php
@@ -41,15 +41,45 @@ class RuleHandler implements ConfigurationHandlerInterface
         $ruleArray = $entity->jsonSerialize();
         unset($ruleArray['id'], $ruleArray['uuid']);
 
-        // Replace IDs with slugs where applicable
-        if (isset($ruleArray['source_id']) && isset($mappings['source']['idToSlug'][$ruleArray['source_id']])) {
-            $ruleArray['source_id'] = $mappings['source']['idToSlug'][$ruleArray['source_id']];
-        }
-        if (isset($ruleArray['target_id']) && isset($mappings['source']['idToSlug'][$ruleArray['target_id']])) {
-            $ruleArray['target_id'] = $mappings['source']['idToSlug'][$ruleArray['target_id']];
+        // Handle nested configuration structures
+        if (isset($ruleArray['configuration']) && is_array($ruleArray['configuration'])) {
+            $ruleArray['configuration'] = $this->convertIdsToSlugs($ruleArray['configuration'], $mappings);
         }
 
         return $ruleArray;
+    }
+
+    /**
+     * Recursively convert IDs to slugs in configuration arrays
+     *
+     * @param array $config The configuration array to process
+     * @param array $mappings The mappings array containing idToSlug mappings
+     * @return array The processed configuration with IDs converted to slugs
+     */
+    private function convertIdsToSlugs(array $config, array $mappings): array
+    {
+        $entityTypes = ['source', 'job', 'endpoint', 'mapping', 'register', 'schema'];
+        
+        foreach ($config as $key => $value) {
+            if (is_array($value)) {
+                // Recursively process nested arrays
+                $config[$key] = $this->convertIdsToSlugs($value, $mappings);
+            } else {
+                // Check if the key is an entity reference
+                foreach ($entityTypes as $type) {
+                    // Check for exact match (e.g., 'source')
+                    if ($key === $type && isset($mappings[$type]['idToSlug'][$value])) {
+                        $config[$key] = $mappings[$type]['idToSlug'][$value];
+                    }
+                    // Check for ID suffix (e.g., 'sourceId')
+                    if (str_ends_with($key, $type . 'Id') && isset($mappings[$type]['idToSlug'][$value])) {
+                        $config[$key] = $mappings[$type]['idToSlug'][$value];
+                    }
+                }
+            }
+        }
+
+        return $config;
     }
 
     /**
